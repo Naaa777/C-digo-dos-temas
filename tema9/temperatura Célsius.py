7799a8263d9bb8ca177e2f79c76987d846dca4de
Naaa777 <cardosonicolas1533@gmail.com>
2024-09-16 09:57:13 -0300

    Add files via upload

---
+++ tema9/temperatura Célsius.py
@@ -0,0 +1,11 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Declarando a temperatura em Célsius
+... celsius = 25.0
+... #calculando a temperatura em Fahrenheit
+... fahrenheit = (celsius * 9/5) + 32
+... 
+... #Exibindo os resultados
+... print ("Temperatura em Célsius:", celsius)
+... Temperatura em Célsius: 25.0
+... print ("Temperatura em Fahrenheit:", fahrenheit)
